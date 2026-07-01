0f8c2e6cf81f633d1ebcd0933edfbbf05db12813
jotavgalves <jvgacontato@gmail.com>
2026-07-01 15:10:59 -0300

    Inject customer checkout on all public HTML pages

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,7 @@
 const CACHE_BUST_SCRIPT = '<script src="/assets/catalog-cache-bust.js?v=4"></script>';
+const CUSTOMER_CHECKOUT_SCRIPT = '<script src="/assets/customer-checkout.js?v=1" defer></script>';
 const CACHE_BUST_RE = /<script\s+src=["']\/assets\/catalog-cache-bust\.js\?v=[^"']+["']><\/script>/g;
+const CUSTOMER_CHECKOUT_RE = /<script\s+src=["']\/assets\/customer-checkout\.js\?v=[^"']+["']\s+defer><\/script>/g;
 
 export async function onRequest(context) {
   const url = new URL(context.request.url);
@@ -15,11 +17,11 @@ export async function onRequest(context) {
   }
 
   let html = await assetResponse.text();
-  if (CACHE_BUST_RE.test(html)) {
-    html = html.replace(CACHE_BUST_RE, CACHE_BUST_SCRIPT);
-  } else {
-    html = html.replace('</head>', `${CACHE_BUST_SCRIPT}</head>`);
-  }
+  if (CACHE_BUST_RE.test(html)) html = html.replace(CACHE_BUST_RE, CACHE_BUST_SCRIPT);
+  else html = html.replace('</head>', `${CACHE_BUST_SCRIPT}</head>`);
+
+  if (CUSTOMER_CHECKOUT_RE.test(html)) html = html.replace(CUSTOMER_CHECKOUT_RE, CUSTOMER_CHECKOUT_SCRIPT);
+  else html = html.replace('</head>', `${CUSTOMER_CHECKOUT_SCRIPT}</head>`);
 
   const headers = new Headers(assetResponse.headers);
   headers.delete('content-length');
