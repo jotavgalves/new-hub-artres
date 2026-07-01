3ba8634516565eeafd654c14768c554908554a96
jotavgalves <jvgacontato@gmail.com>
2026-07-01 09:59:15 -0300

    Add admin logout endpoint

---
+++ functions/api/admin/logout.js
@@ -0,0 +1,6 @@
+import { json } from "../_config.js";
+import { clearSessionCookie } from "./_auth.js";
+
+export async function onRequestPost() {
+  return json({ ok: true }, 200, { "Set-Cookie": clearSessionCookie() });
+}
