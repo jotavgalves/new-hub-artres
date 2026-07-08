200b8420fdf140e7add7cb8b6464461ddde96402
jotavgalves <jvgacontato@gmail.com>
2026-07-08 14:22:34 -0300

    Atualiza versao da busca auxiliar do catalogo

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,6 @@
 const CACHE_BUST_SCRIPT = '<script src="/assets/catalog-cache-bust.js?v=7"></script>';
 const CUSTOMER_CHECKOUT_SCRIPT = '<script src="/assets/customer-checkout.js?v=6" defer></script>';
-const CATALOG_DRIVE_SEARCH_SCRIPT = '<scr' + 'ipt src="/assets/catalog-drive-search.js?v=2" defer></scr' + 'ipt>';
+const CATALOG_DRIVE_SEARCH_SCRIPT = '<scr' + 'ipt src="/assets/catalog-drive-search.js?v=3" defer></scr' + 'ipt>';
 const HERO_LOGO_CENTER_STYLE = '<style id="heroLogoCenterStyle">.brand .logo{margin-left:auto;margin-right:auto}</style>';
 const ORDER_TOOLS_SCRIPT = '<script src="/assets/order-action-tools.js?v=4" defer></script>';
 const CACHE_BUST_RE = /<script\s+src=["']\/assets\/catalog-cache-bust\.js\?v=[^"']+["']><\/script>/g;
