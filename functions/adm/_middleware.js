d13dbdd140713d62083115bbe1b0c0fbe5d3937b
jotavgalves <jvgacontato@gmail.com>
2026-09-11 12:51:15 -0300

    feat: carregar configuração do Retangular 1x2 no admin

--- functions/adm/_middleware.js
+++ functions/adm/_middleware.js
@@ -1,5 +1,6 @@
 const PEDIDOS_SIDEBAR_SCRIPT = '<script id="adminPedidosSidebarScript" src="/assets/pedidos-sidebar.js?v=3" defer></script>';
 const PAINEL_ROMANO_ADMIN_SCRIPT = '<script id="adminPainelRomanoScript" src="/assets/admin-painel-romano.js?v=1" defer></script>';
+const RETANGULAR_1X2_ADMIN_SCRIPT = '<script id="adminRetangular1x2Script" src="/assets/admin-retangular-1x2.js?v=1" defer></script>';
 
 export async function onRequest(context) {
   const response = await context.next();
@@ -10,6 +11,7 @@ export async function onRequest(context) {
   const scripts = [];
   if (!html.includes('/assets/pedidos-sidebar.js')) scripts.push(PEDIDOS_SIDEBAR_SCRIPT);
   if (!html.includes('/assets/admin-painel-romano.js')) scripts.push(PAINEL_ROMANO_ADMIN_SCRIPT);
+  if (!html.includes('/assets/admin-retangular-1x2.js')) scripts.push(RETANGULAR_1X2_ADMIN_SCRIPT);
   if (scripts.length) html = html.replace('</body>', `${scripts.join('')}</body>`);
 
   const headers = new Headers(response.headers);
